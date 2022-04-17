aa15dda074c108ed8bc4a61ae1d532a9c8c056a8
FranciscoN3to <francisco2012jf@gmail.com>
2022-04-16 22:23:01 -0300

    chore: create config apis

---
+++ src/providers/api/clickup.api.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+ 
+const clickupAPI = axios.create({
+	baseURL: process.env.CLICKUP_API_URL   
+});
+
+clickupAPI.interceptors.request.use(async (axiosConfig) => {
+	axiosConfig.headers.Authorization = process.env.CLICKUP_TOKEN;
+	return axiosConfig;
+});
+
+clickupAPI.interceptors.response.use((axiosConfig) => {
+
+    if(axiosConfig.data?.data) {
+        axiosConfig.data = axiosConfig.data.data;
+    }
+
+    return axiosConfig;
+})
+
+
+export default clickupAPI;
---
+++ src/providers/api/google.api.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { auth } from "../auth/google";
+
+const googleAPI = axios.create({
+	baseURL: process.env.GOOGLE_API_URL,    
+});
+
+
+
+googleAPI.interceptors.request.use(async (axiosConfig) => {
+	axiosConfig.headers.Authorization = `Bearer ${process.env.token}`;
+	return axiosConfig;
+});
+
+googleAPI.interceptors.response.use(null, async (error) => {
+	if (error?.response) {
+		if ([403, 401].includes(error.response.status)) {
+			const authentication = await auth();
+			process.env.token = authentication.access_token;
+			error.config.headers.Authorization = `Bearer ${process.env.token}`;
+			return axios(error.config);
+		}
+	}
+	throw error;
+});
+
+export default googleAPI;
